c12b7b70921f5f476cecc840dce7950b51f1bf32
Yassin Bagane <yassin.bagane2@gmail.com>
2023-02-23 01:48:51 +0100

    bug fix

--- client/src/components/HashtagInterface.jsx
+++ client/src/components/HashtagInterface.jsx
@@ -19,7 +19,7 @@ const HashtagInterface = () => {
       : "";
 
   const getHashtags = async (e) => {
-    console.log(keyword)
+    console.log(keyword);
     setHashtags([]);
     setIsLoading(true);
 
@@ -38,24 +38,26 @@ const HashtagInterface = () => {
   return (
     <div className="w-[620px] min-h-[620px] relative p-6 mb-6 bg-white rounded-md">
       <div className="flex items-center gap-4 flex-col">
-          <h1 className="text-2xl text-gray-900 font-bold">
-            Hashtags generators
-          </h1>
-          <div className="flex items-center gap-2">
-            <input
-              className="p-3  placeholder:text-sm border rounded-lg focus:outline-indigo-600 w-[70%] z-20 shadow-sm"
-              placeholder="ex. Summer"
-              onChange={() => setKeyword(e.target.value)}
-              type="text"
-            />
-            <button className="rounded-md bg-indigo-600 p-3 text-white font-semibold" onClick={getHashtags}>Generate</button>
-          </div>
-
+        <h1 className="text-2xl text-gray-900 font-bold">
+          Hashtags generators
+        </h1>
+        <div className="flex items-center gap-2">
+          <input
+            className="p-3  placeholder:text-sm border rounded-lg focus:outline-indigo-600 w-[70%] z-20 shadow-sm"
+            placeholder="ex. Summer"
+            onChange={(e) => setKeyword(e.target.value)}
+            type="text"
+          />
+          <button
+            className="rounded-md bg-indigo-600 p-3 text-white font-semibold"
+            onClick={getHashtags}
+          >
+            Generate
+          </button>
+        </div>
       </div>
-      
-      
-      <div>
 
+      <div>
         <img
           className="w-24 absolute top-2 right-2"
           src={hashtag}
@@ -63,17 +65,13 @@ const HashtagInterface = () => {
         />
       </div>
 
-      
-      
-      {
-        loading || hashtagsElements.length > 0 && (
-        <div className="w-[400px] mx-auto rounded-md bg-gray-50 mt-3 shadow-sm border-2 p-4">
-          {loading} 
-          <ul>{hashtagsElements}</ul>
-        </div> )
-      }
-      
-      
+      {loading ||
+        (hashtagsElements.length > 0 && (
+          <div className="w-[400px] mx-auto rounded-md bg-gray-50 mt-3 shadow-sm border-2 p-4">
+            {loading}
+            <ul>{hashtagsElements}</ul>
+          </div>
+        ))}
     </div>
   );
 };
